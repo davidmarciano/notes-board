9bfd124b49601424f591a16c3ede001b73b1c599
davidmarciano <david.marciano85@gmail.com>
2023-01-29 20:12:58 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback } from 'react';
+import styled from 'styled-components';
+import { NotesBoard, Modal } from './components';
+import { useModal } from './components/Modal/hooks';
+import useNotes from './components/NotesBoard/useNotes';
 
-function App() {
+const Wrapper = styled.div`
+  width: 100%;
+  height: fit-content;
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+  padding: 20px;
+`;
+
+const Title = styled.h1`
+  margin: 0px;
+`;
+
+const App = () => {
+  const { notes, postNote, deleteNote, selectedNote, setSelectedNoteId } = useNotes();
+  const { isModalOpen, showModal, hideModal } = useModal();
+
+  const onModalClose = useCallback(() => {
+    setSelectedNoteId('');
+    hideModal();
+  }, [setSelectedNoteId, hideModal]);
+
+  const onNoteClick = useCallback(
+    (noteId: string) => {
+      setSelectedNoteId(noteId);
+      showModal();
+    },
+    [setSelectedNoteId, showModal]
+  );
+
+  const onPost = useCallback((author: string, message: string, id?: string) => {
+    postNote(author, message, id);
+    hideModal();
+  }, [postNote, hideModal]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Title>Welcome to post board</Title>
+      <NotesBoard 
+        notes={notes}
+        onAddClick={showModal}
+        onNoteClick={onNoteClick}
+        onDeleteClick={deleteNote}
+      />
+      <Modal 
+        data={selectedNote}
+        isOpen={isModalOpen}
+        onPost={onPost}
+        onClose={onModalClose}
+      />
+    </Wrapper>
   );
 }
 
